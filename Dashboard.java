fa03620f7f2020de1c3436ff11f7cd254f0fa77b
sing1179 <143670921+sing1179@users.noreply.github.com>
2025-05-22 18:38:49 -0400

    Create Dashboard.java

---
+++ Dashboard.java
@@ -0,0 +1,11 @@
+package frontend;
+
+public class Dashboard {
+    public void showRepoSummary(String summary) {
+        System.out.println("Repo Summary: " + summary);
+    }
+
+    public void showPRSuggestion(String suggestion) {
+        System.out.println("PR Suggestion: " + suggestion);
+    }
+}
